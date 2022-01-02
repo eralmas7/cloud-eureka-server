afde0769ecb015beef74691126efb83ad0a6f424
eralmas7 <eralmas7@yahoo.com>
2022-01-02 22:27:59 +0530

    Fixed warnings + updated configuration.

--- src/test/java/com/example/eureka/server/EurekaServerApplicationTest.java
+++ src/test/java/com/example/eureka/server/EurekaServerApplicationTest.java
@@ -2,7 +2,6 @@ package com.example.eureka.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 public class EurekaServerApplicationTest {
